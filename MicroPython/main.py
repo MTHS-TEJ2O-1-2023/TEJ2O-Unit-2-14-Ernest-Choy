2465d359e2aa5114c0a635e25e0d8bfff047ef07
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-08 18:27:27 +0000

    pyver to do

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,7 +4,31 @@ Created on: Sep 2020
 This module is a Micro:bit MicroPython program
 """
 
-from microbit import *
+from microbit import*
 
+# variables
+pixel_count = 0 
 
-display.scroll("Hello, World!")
+# setup
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        loopCount = 0
+
+        while pixel_count <= 5: 
+            display.set_pixel(pixel_count ,pixel_count, 255)
+            pixel_count = pixel_count + 1
+
+    if button_b.is_pressed():
+        display.clear()
+        loopCount = 5
+
+        while pixel_count >= 0: 
+            display.set_pixel(pixel_count ,pixel_count, 255)
+            pixel_count = pixel_count - 1
+
+    display.clear()
+    display.show(Image.SAD)
